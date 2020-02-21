acd4f23be46cea82d407cb0cb6308b19d695e3c4
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-02-21 00:59:00 -0300

    Config RestTemplate para lidar com o eureka

--- src/main/java/br/com/rodrigues/loja/service/CompraService.java
+++ src/main/java/br/com/rodrigues/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package br.com.rodrigues.loja.service;
 
 import br.com.rodrigues.loja.controller.CompraDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,12 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
+
+    @Autowired
+    private RestTemplate client;
+
     public void realizaCompra(CompraDTO compra) {
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(),
+        ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
 
         System.out.println(response.getBody().getEndereco());
