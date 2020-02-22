de66a40ec366f6abb801f650b3c9dbbb00ed8503
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-02-22 10:59:20 -0300

    Client side load balance
    O client tem a inteligência de load balance já implementada

--- src/main/java/br/com/rodrigues/loja/service/CompraService.java
+++ src/main/java/br/com/rodrigues/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.rodrigues.loja.service;
 
 import br.com.rodrigues.loja.controller.CompraDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,14 @@ public class CompraService {
     @Autowired
     private RestTemplate client;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public void realizaCompra(CompraDTO compra) {
         ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
                 HttpMethod.GET, null, InfoFornecedorDTO.class);
+
+        discoveryClient.getInstances("fornecedor").stream().forEach(serviceInstance -> System.out.println(serviceInstance.getPort()));
 
         System.out.println(response.getBody().getEndereco());
     }
