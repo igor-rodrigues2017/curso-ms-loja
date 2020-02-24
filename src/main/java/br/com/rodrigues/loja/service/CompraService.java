c983930c990195655dcd30ae954e93e60aad2a58
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2020-02-23 23:53:09 -0300

    Unificando os logs e tracking as requisições

--- src/main/java/br/com/rodrigues/loja/service/CompraService.java
+++ src/main/java/br/com/rodrigues/loja/service/CompraService.java
@@ -3,21 +3,29 @@ package br.com.rodrigues.loja.service;
 import br.com.rodrigues.loja.client.FornecedorClient;
 import br.com.rodrigues.loja.controller.CompraDTO;
 import br.com.rodrigues.loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompraService {
 
+    private static final Logger log = LoggerFactory.getLogger(CompraService.class);
+
     @Autowired
     private FornecedorClient client;
 
     public Compra realizaCompra(CompraDTO compra) {
-        InfoFornecedorDTO fornecedorDTO = client.getInfoPorEstado(compra.getEndereco().getEstado());
+        String estado = compra.getEndereco().getEstado();
 
+        log.info("Buscando informações do forncedor no estado {}", estado);
+        InfoFornecedorDTO fornecedorDTO = client.getInfoPorEstado(estado);
+
+        log.info("Realizando um pedido");
         InfoPedidoDTO pedido = client.realizaPedido(compra.getItens());
         Compra compraRealizada = new Compra(pedido);
-        compraRealizada.setEnderecoDestino(compra.getEndereco().toString());
+        compraRealizada.setEnderecoDestino(fornecedorDTO.getEndereco());
         return compraRealizada;
     }
 }
